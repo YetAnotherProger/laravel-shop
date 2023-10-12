7048f58a2c2978effa93ed701803c95c719738cb
alx <alx@pop-os.localdomain>
2023-10-12 19:52:26 +0300

    stage 1 homework: add bool and exception(not custom)

--- app/Services/Telegram/TelegramBotApi.php
+++ app/Services/Telegram/TelegramBotApi.php
@@ -2,6 +2,7 @@
 
 namespace App\Services\Telegram;
 
+use Illuminate\Http\Client\RequestException;
 use Illuminate\Support\Facades\Http;
 
 class TelegramBotApi
@@ -9,15 +10,20 @@ class TelegramBotApi
 
     const HOST = 'https://api.telegram.org/bot';
 
+    /**
+     * @throws RequestException
+     */
     public static function sendMessage(
         string $token,
         int $chat_id,
         string $text
-    ): void {
-        Http::get(self::HOST.$token.'/sendMessage', [
+    ): bool {
+        return Http::get(self::HOST.$token.'/sendMessage', [
             'chat_id' => $chat_id,
             'text' => $text,
-        ]);
+        ])->throw()
+            ->successful();
+
     }
 
 }
